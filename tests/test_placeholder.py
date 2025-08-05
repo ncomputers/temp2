a781063f8fd96c97531e6243c1a1f18fde5fa597
ncomputers <ncomputerudr@gmail.com>
2025-08-05 11:38:59 +0530

    Add subtraction test

--- tests/test_placeholder.py
+++ tests/test_placeholder.py
@@ -4,3 +4,8 @@ import pytest
 def test_addition():
     """Ensure basic arithmetic works"""
     assert 1 + 1 == 2
+
+
+def test_subtraction():
+    """Ensure subtraction works"""
+    assert 5 - 3 == 2
